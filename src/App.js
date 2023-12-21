6016a5f56b123369416cb7d90f611efcc829ff04
juber <juberk1010@gmail.com>
2023-12-21 23:02:48 +0530

    show diffrent movies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   
    const fetchMovies = async() => {
-       const res = await fetch(`https://www.omdbapi.com/?s=spider&apikey=66ec4406`);
+       const res = await fetch(`https://www.omdbapi.com/?s=movies&apikey=66ec4406`);
        const data = await res.json();
        if(data.Search) setMovies(data.Search);
      }
